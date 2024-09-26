097c99be8f4ccdd626345d9ad425adde602bb402
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-09-26 18:40:39 +0530

    Time: 1843 ms (5%), Space: 452.1 MB (7.39%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    vector<vector<int>> dp;
+
+    bool palindrome(string &s,int l,int r,pair<int,int> &a,vector<vector<int>> &dp){
+        int n = r-l+1;
+        for(int i = 0;i < (n/2);i++){
+            if(s[l+i] != s[l+n-i-1]) {
+                dp[l][r] = 0;
+                return false;
+                }
+        }
+
+        dp[l][r] = 1;
+        if(a.second - a.first < r-l) a = {l,r};
+        return true;
+    }
+
+    void recPalindrome(string &s,int i,int j,pair<int,int> &p,vector<vector<int>> &dp){
+        if(j <= i) return;
+        if(dp[i][j] == -1) palindrome(s,i,j,p,dp);
+        if(dp[i+1][j] == -1) recPalindrome(s,i+1,j,p,dp);
+        if(dp[i][j-1] == -1) recPalindrome(s,i,j-1,p,dp);
+        return;
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.length();
+        vector< vector<int> > dp;
+        for(int i =0;i<n;i++){
+            vector<int> temp(n,-1);
+            dp.push_back(temp);
+        }
+        pair<int,int> maxran = {0,0};
+        recPalindrome(s,0,n-1,maxran,dp);
+        return s.substr(maxran.first,maxran.second-maxran.first+1);
+    }
+};

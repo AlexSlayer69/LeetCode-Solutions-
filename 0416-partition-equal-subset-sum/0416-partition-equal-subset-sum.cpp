f8450459e6250390c220007944a62316357a745d
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-09-27 20:01:24 +0530

    Time: 145 ms (57.52%), Space: 12.9 MB (91.96%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -5,18 +5,19 @@ public:
         int total = 0,n = nums.size();
         for(int i =0;i<n;i++) total += nums[i];
         if((total%2) == 1 || n == 1) return false;
-        vector<vector<bool>> dp (n, vector<bool>((total/2) +1,false));
+        vector<bool> prev((total/2) +1,false) , curr((total/2) +1,false);
         
-        for(int i = 0;i<n;i++) dp[i][0] = true;
-        if(nums[0] <= total/2) dp[0][nums[0]] = true;
+        prev[0] = true,curr[0] = true;
+        if(nums[0] <= total/2) prev[nums[0]] = true;
 
         for(int i = 1;i<n;i++){
             for(int j = 1; j <= total/2;j++){
-                if(dp[i-1][j]) dp[i][j] = true; 
-                else if(j >= nums[i] && dp[i-1][j-nums[i]]) dp[i][j] = true;
-                else dp[i][j] = false;
-                if(j == total/2 && dp[i][j]) return true;
+                if(prev[j]) curr[j] = true; 
+                else if(j >= nums[i] && prev[j-nums[i]]) curr[j] = true;
+                else curr[j] = false;
+                if(j == total/2 && curr[j]) return true;
             }
+            prev = curr;
         }
 
         return false;

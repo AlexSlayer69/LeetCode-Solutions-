be207e1f84db0cff14052c89554dd14f9ae0ac38
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-09-28 16:41:36 +0530

    Time: 92 ms (52.37%), Space: 22.6 MB (52.92%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -4,25 +4,18 @@ public:
         int n1 = word1.size();
         int n2 = word2.size();
         if(n1 != n2) return false;
-        unordered_map<char,int> m1;
-        unordered_map<char,int> m2;
-        vector<int> v1;
-        vector<int> v2; 
-        for(auto it : word1) m1[it]++;
-        for(auto it : word2) m2[it]++;
-        for(auto it : m1) {
-            if(m2.find(it.first) == m2.end())
-                return false;
-        }
-        for(auto it : m2) {
-            if(m1.find(it.first) == m1.end())
-                return false;
-        }
-        for(auto it : m1) v1.push_back(it.second);
-        for(auto it : m2) v2.push_back(it.second);
+        set<char> s1;
+        set<char> s2;
+        vector<int> v1(26,0);
+        vector<int> v2(26,0); 
+        s1.insert(word1.begin(),word1.end());
+        s2.insert(word2.begin(),word2.end());
+        if(s1 != s2) return false;
+        for(int i = 0;i < n1;i++) v1[word1[i] - 'a']++;
+        for(int i = 0;i < n2;i++) v2[word2[i] - 'a']++;
         sort(v1.begin(),v1.end());
         sort(v2.begin(),v2.end());
-
-        return (v1 == v2) ? true : false;
+        if(v1 != v2) return false;
+        return true;
     }
 };

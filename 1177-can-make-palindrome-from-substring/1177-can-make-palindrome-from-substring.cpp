d6fb00a0e46a73a1d0f017201f5e51d30ebbad0e
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-04 14:37:13 +0530

    Time: 273 ms (60.38%), Space: 154.5 MB (40.81%) - LeetHub

--- 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -3,15 +3,17 @@ public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
         int n = queries.size();
         vector<bool> ans(n,false);
+        vector< vector<int> > pf;
+        vector<int> temp(26,0);
+        pf.push_back(temp);
+        for(int i =0;i < s.length();i++){
+            temp[s[i] - 'a']++;
+            pf.push_back(temp);
+        }
         for(int i =0;i<n;i++){
-            unordered_map<int,int> mp;
-            string k = s.substr(queries[i][0],queries[i][1] - queries[i][0] + 1);
-            for(auto c : k){
-                mp[c]++;
-            }
             int l = 0;
-            for(auto it : mp){
-                if(it.second%2) l++;
+            for(int j =0;j<26;j++){
+                l += (pf[queries[i][1] + 1][j] - pf[queries[i][0]][j])%2;
             }
             if((queries[i][1] - queries[i][0] + 1)%2 && (l-1)/2 <= queries[i][2]) ans[i] = true;
             if((queries[i][1] - queries[i][0] + 1)%2 == 0 && l/2 <= queries[i][2]) ans[i] = true;

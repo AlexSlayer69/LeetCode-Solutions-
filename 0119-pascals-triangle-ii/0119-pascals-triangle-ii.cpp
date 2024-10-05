2cc1a2e285e9842fcf3d4a12a9e0c5f64d4e1207
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-05 18:50:09 +0530

    Time: 18 ms (9.69%), Space: 7.9 MB (31.12%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    int nCr(int n ,int r){
+        double prod = 1.0;
+        for(int i = 0;i<r;i++){prod *= (n-i)*1.0/(r-i);cout << "r: " <<  r << " " << prod <<   " " << (n-i)*1.0/(r-i) << endl;}
+        return (int) round(prod);
+    }
+
+    vector<int> getRow(int rowIndex) {
+        vector<int> v(rowIndex+1,0);
+        for(int i =0;i<=rowIndex;i++){
+            v[i] = nCr(rowIndex,i);
+        }
+        return v;
+    }
+};

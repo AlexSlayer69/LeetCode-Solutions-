af93ca25e243d512d480e5997dab2c189a9e7a80
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-05 19:19:56 +0530

    Time: 4 ms (77.89%), Space: 15.6 MB (69.54%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,16 @@
+bool compare(int a, int b) {
+    string sa = to_string(a);
+    string sb = to_string(b);
+    return ((sa + sb) > (sb + sa)) ? true : false;
+}
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), compare);
+        if (nums[0] == 0) return "0";
+        string ans = "";
+        for (auto it : nums) ans += to_string(it);
+        return ans;
+    }
+};

c944a31eefe15ab33444cb44cea6e01b3f6c52b0
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-06 15:59:07 +0530

    Time: 69 ms (87.92%), Space: 70.4 MB (98.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,7 +5,7 @@ public:
     for(int i = 0;i<nums.size();i++){
         sum += nums[i];
         maxi = max(sum,maxi);
-        if(sum < 0) sum = 0;
+        sum = max(sum,0);
     }
      return maxi;
     }

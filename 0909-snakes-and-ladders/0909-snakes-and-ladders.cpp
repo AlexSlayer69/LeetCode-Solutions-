d1e15e17a100e25ca8da9286210f3b9d81efc04d
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-10 00:56:25 +0530

    Time: 9 ms (84.81%), Space: 16.5 MB (50.37%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
-    int snakesAndLadders(vector<vector<int>> &board) {
-        int n = board.size(), lbl = 1;
+   int dij(unordered_map<int, vector<pair<int, int>>> &god, vector<int> &dist, int n) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, 1});
+    dist[1] = 0;
+    
+    while (!pq.empty()) {
+        int curr_dist = pq.top().first;
+        int node = pq.top().second;     
+        pq.pop();
+        
+        if (node == n * n) {
+            return curr_dist;
+        }
+
+        for (auto neighbor : god[node]) {
+            int neighbor_node = neighbor.second;
+            int edge_weight = neighbor.first;
+            int new_dist = curr_dist + edge_weight;
+            if (new_dist < dist[neighbor_node]) {
+                dist[neighbor_node] = new_dist;
+                pq.push({new_dist, neighbor_node});
+            }
+        }
+    }
+    return -1;
+}
+
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size();
+        int l = 1;
         vector<pair<int, int>> cells(n*n+1);
         vector<int> columns(n);
         iota(columns.begin(), columns.end(), 0);
+
         for (int row = n - 1; row >= 0; row--) {
-            for (int column : columns) {
-                cells[lbl++] = {row, column};
+            for (auto col : columns) {
+                cells[l++] = {row, col};
             }
             reverse(columns.begin(), columns.end());
         }
+
+
         vector<int> dist(n*n+1, -1);
         dist[1] = 0;
         queue<int> q;
@@ -19,11 +50,11 @@ public:
             int curr = q.front();
             q.pop();
             for (int next = curr + 1; next <= min(curr+6, n*n); next++) {
-                auto [row, column] = cells[next];
-                int destination = board[row][column] != -1 ? board[row][column] : next;
-                if (dist[destination] == -1) {
-                    dist[destination] = dist[curr] + 1;
-                    q.push(destination);
+                auto [row, col] = cells[next];
+                int d = board[row][col] != -1 ? board[row][col] : next;
+                if (dist[d] == -1) {
+                    dist[d] = dist[curr] + 1;
+                    q.push(d);
                 }
             }
         }

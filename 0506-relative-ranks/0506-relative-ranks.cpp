32b7085480b0cb0d368b66db87f2494e2fb7cd63
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-11 23:00:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>> pq;
+        int n = score.size();
+        for(int i = 0;i<n;i++){
+            pq.push({score[i],i});
+        }
+
+        vector<string> answer(n,"");
+        int sc = 1;
+        while(!pq.empty()){
+            auto [s,i] = pq.top();
+            pq.pop();
+            if(sc == 1) answer[i] = "Gold Medal";
+            else if (sc == 2) answer[i] = "Silver Medal";
+            else if(sc == 3) answer[i] = "Bronze Medal";
+            else{
+                answer[i] = (sc + '0');
+            }
+            sc++;
+        }
+        return answer;
+    }
+};

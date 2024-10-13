35724efa59bf5768d23092fa4c9fe29a8ba679db
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-14 00:39:15 +0530

    Time: 1083 ms (5.05%), Space: 158.3 MB (10.81%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        set<int> s(nums.begin(),nums.end());
+        int n = nums.size();
+        if(valueDiff==0 and n == s.size()) return false;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<=i+indexDiff && j<n;j++){
+            if(abs(nums[i]-nums[j])<=valueDiff)return true;
+            }
+        }
+        return false;
+    }
+};

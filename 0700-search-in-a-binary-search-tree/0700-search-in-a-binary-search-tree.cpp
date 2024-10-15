4a8dc29a983061f2684ecb2514250b7ab953c41e
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-15 20:12:56 +0530

    Time: 35 ms (22.98%), Space: 33.4 MB (96.26%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -15,7 +15,7 @@ public:
         if(root == NULL) return NULL;
         if(root->val == val) return root;
         if(val < root->val) return searchBST(root->left,val);
-        else if(val > root->val) return searchBST(root->left,val);
+        else if(val > root->val) return searchBST(root->right,val);
         else return NULL;
     }
 };

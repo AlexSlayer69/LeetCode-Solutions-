d87e2eef2bf695d63dc34f11e93efbe0ad2a8404
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-17 23:58:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int i = 1;
+        while(i*i != x && !((i+1)*(i+1) > x && i*i < x)){
+            i = (i + x/i)/2;
+        }
+        return i; 
+    }
+};

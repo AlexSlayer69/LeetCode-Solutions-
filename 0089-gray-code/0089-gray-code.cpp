6f54d1ee71810de56e8e0e5ded18b0230baf84f7
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-20 00:04:55 +0530

    Time: 3 ms (86.23%), Space: 13.1 MB (89.42%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> gray;
+        for(int i =0;i < (1 << n);i++){
+            gray.push_back(i ^ (i >> 1));
+        }
+        return gray;
+    }
+};

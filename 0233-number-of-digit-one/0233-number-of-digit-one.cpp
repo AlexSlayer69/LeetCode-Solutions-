f21bd5b345c0d170a6995d8f5805a419cfcbf2f4
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-20 01:40:33 +0530

    Time: 2 ms (52.84%), Space: 7.7 MB (24.56%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countDigitOne(int n) {
+        if(n == 0) return 0;
+        if(n < 10) return 1;
+        int digit = n;
+        int ndigits = 0;
+        while(n){
+            ndigits++;
+            n /= 10;
+        }
+        n = digit;
+        int p = pow(10,ndigits-1);
+        int fd  = n/p;
+        int ans = countDigitOne(n%p) + fd*countDigitOne(p -1);
+        cout << ans;
+        if(fd == 1) ans += n%p+1;
+        else ans += p;  
+        return ans;
+    }
+};

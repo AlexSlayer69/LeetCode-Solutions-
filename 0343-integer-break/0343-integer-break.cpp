dcd21787047a5c2942096a84c211153b7cde0f68
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-20 00:04:50 +0530

    Time: 0 ms (100%), Space: 7.4 MB (78.94%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        int maxi = 1;
         if(n == 3) return 2;
+        int maxi = 1;
         for(int k = 1; k < (n+1)/2;k++){
-            // int rem = n%k;
-            // int same = n/k;
             int candi = (int) pow(n/k+1,n%k) * pow(n/k,k-(n%k));
             maxi = max(candi,maxi);
         }

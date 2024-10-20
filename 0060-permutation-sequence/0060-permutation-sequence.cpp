d4730213e7fb37f7cf58d7acfea4e6928ad7a321
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-21 00:13:19 +0530

    Time: 45 ms (17.35%), Space: 7.4 MB (97.95%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> a(n,0);
-        for(int i = 1;i<=n;i++) a[i-1] = i;
-
-        for(int i = 1;i<k;i++) next_permutation(a.begin(),a.end());
-
         string s = "";
-        for(auto it : a) s += (it + '0');
+        for(int i = 1;i<=n;i++) s += (i +'0');
+        for(int i = 1;i<k;i++) next_permutation(s.begin(),s.end());
         return s;
     }
 };

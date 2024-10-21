4d1d8e26b9e914fd13167371871d155a1075adaf
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-22 04:44:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> st; int res = 0, sign = 1, num = 0;
-        for (char c : s) {
-            if (isdigit(c)) num = num * 10 + (c - '0');
-            else if (c == '+' || c == '-') res += sign * num, sign = (c == '+') ? 1 : -1, num = 0;
-            else if (c == '(') st.push(res), st.push(sign), res = 0, sign = 1;
-            else if (c == ')') res += sign * num, res *= st.top(), st.pop(), res += st.top(), st.pop(), num = 0;
+        stack<int> st;
+        int curr = 0,res = 0,sign = 1;
+        for(char c : s){
+            if(isdigit(c)) curr = (curr * 10) + (c - '0');
+            else if(c == '+' || c == '-'){
+                res += sign*curr;
+                if( c == '+') sign =1;
+                else sign = -1;
+                curr = 0;
+            }
+            else if(c == '('){
+                st.push(res);
+                st.push(sign);
+                res = 0;
+                sign = 1;
+            }
+            else if(c == ')'){
+                res += sign*curr;
+                res *= st.top();
+                st.pop();
+                res += st.top();
+                st.pop();
+                curr = 0;
+            }
+            cout << "c: " << c << ", res: " << res << ", num: " << curr << ", sign: " << sign << endl;  
         }
-        return res + sign * num;
+        cout  << "res: " << res << ", num: " << curr << ", sign: " << sign << endl;  
+        return res + sign*curr;
     }
 };

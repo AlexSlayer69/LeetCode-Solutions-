1dd3ddec0d09bd747955ebfdd9d8d18cd2a2cc7a
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-23 00:36:18 +0530

    Time: 2 ms (63.1%), Space: 9.4 MB (21.24%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int dp[21][21]; 
     
-    int optimalPlay(vector<int>& nums, int l, int r) {
+    int optimalPlay(vector<int>& nums, int l, int r,vector<vector<int>> &dp) {
         if (l > r) return 0;
         
         if (dp[l][r] != -1) return dp[l][r];  
-        int pickLeft = nums[l] - optimalPlay(nums, l + 1, r);  
-        int pickRight = nums[r] - optimalPlay(nums, l, r - 1); 
+        int pickLeft = nums[l] - optimalPlay(nums, l + 1, r,dp);  
+        int pickRight = nums[r] - optimalPlay(nums, l, r - 1,dp); 
         
         dp[l][r] = max(pickLeft, pickRight);  
         return dp[l][r];
@@ -15,8 +14,7 @@ public:
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        memset(dp, -1, sizeof(dp)); 
-    
-        return optimalPlay(nums, 0, n - 1) >= 0;
+        vector<vector<int>> dp(21, vector<int> (21,-1));
+        return optimalPlay(nums, 0, n - 1,dp) >= 0;
     }
 };

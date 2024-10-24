061bcdf6fee1219a060cd639b063009bb03e5fac
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-24 23:12:08 +0530

    Time: 4 ms (31.9%), Space: 13 MB (5.33%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     unordered_map<string,int> dp;
     int numDecodings(string s) {
-        if((s.size() == 1 && s[0] == '0')) return 0;
-        if(s.size() == 1 || s.size() == 0) return 1;
+        int n = s.size();
+        if(n == 1 && s[0] == '0') return 0;
+        if(n == 1 || n == 0) return 1;
         else { 
             if(dp.find(s) != dp.end()) return dp[s];
             if(s[0] == '0') {
@@ -11,9 +12,9 @@ public:
                 return dp[s];
             }
             int ans = 0;
-            ans = numDecodings(s.substr(1,s.size()-1));
+            ans = numDecodings(s.substr(1,n-1));
             int k = stoi(s.substr(0,2));
-            if(k > 0 && k < 27)  ans += numDecodings(s.substr(2,s.size()-2));
+            if(k > 0 && k < 27)  ans += numDecodings(s.substr(2,n-2));
             dp[s] = ans;
             return dp[s];    
         }

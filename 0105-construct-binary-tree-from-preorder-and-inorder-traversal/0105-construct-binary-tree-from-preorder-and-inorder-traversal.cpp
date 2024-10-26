e1d68dfe2212d4da8659fc66aed7358ac397e73b
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-26 18:32:00 +0530

    Time: 25 ms (13.73%), Space: 73.9 MB (9.82%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if(preorder.size() == 0) return NULL;
+        TreeNode* root = new TreeNode(preorder[0]);
+        if(preorder.size() == 1) return root;
+        
+        int i = find(inorder.begin(),inorder.end(),preorder[0])- inorder.begin();
+    
+        vector<int> lp = vector<int>(preorder.begin()+1,preorder.begin()+i+1);
+        vector<int> rp = vector<int>(preorder.begin()+i+1,preorder.end());
+        vector<int> li = vector<int>(inorder.begin(),inorder.begin()+i);
+        vector<int> ri = vector<int>(inorder.begin()+i+1,inorder.end());
+
+        // cout << "lp" << endl;
+        // for(auto it : lp) cout << it << " ";
+        // cout << endl;
+        // cout << "rp" << endl;
+        // for(auto it : rp) cout << it << " ";
+        // cout << endl;
+        // cout << "li" << endl;
+        // for(auto it : li) cout << it << " ";
+        // cout << endl;
+        // cout << "ri" << endl;
+        // for(auto it : ri) cout << it << " ";
+        // cout << endl;
+
+        root->left = buildTree(lp,li);
+        root->right = buildTree(rp,ri);
+        return root;
+    }
+};

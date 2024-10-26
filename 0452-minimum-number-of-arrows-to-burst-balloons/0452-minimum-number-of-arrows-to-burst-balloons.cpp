eda30ed27bb3908fc682b4dc8c27b068d548b1ee
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-26 19:02:48 +0530

    Time: 51 ms (97.79%), Space: 93.6 MB (38.06%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -5,7 +5,9 @@ public:
     }
 
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end(),compare);
+        sort(points.begin(),points.end(),[](const vector<int>& v1, const vector<int>& v2) {
+            return v1[1] < v2[1];
+        });
         int cnt = 1;
         int end = points[0][1];
         for(int i = 1;i<points.size();i++){

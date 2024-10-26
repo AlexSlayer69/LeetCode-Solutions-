d4fba8f59767325cb4b042e295dd1fe8cda1f8c6
Akul V Jha <94447014+AlexSlayer69@users.noreply.github.com>
2024-10-26 19:01:08 +0530

    Time: 2071 ms (8.89%), Space: 545.9 MB (8.24%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     static bool compare(vector<int> a, vector<int> b){
-        // if(a[0] < b[0]){
-        //     return true;
-        // }
-        // else if(a[0] > b[0]) return false;
-        // else{
-        //     return (a[1] <= b[1]) ? true : false;
-        // }
         return (a[1] < b[1]);
     }
 
@@ -15,17 +8,11 @@ public:
         sort(points.begin(),points.end(),compare);
         int cnt = 1;
         int end = points[0][1];
-        int tmp = 1;
         for(int i = 1;i<points.size();i++){
             if(points[i][0] > end){
                 cnt++;
-                tmp = 1;
                 end = points[i][1];
             }
-            else{
-                end = min(end,points[i][1]);
-                tmp++;
-            }  
         }
         return cnt;
     }
